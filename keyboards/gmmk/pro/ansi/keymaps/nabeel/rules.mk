307ef51f7bc4973f49bd4cd8975487fbe3b76d1a
Nabeel Shahzad <nshahzad@gmail.com>
2022-04-21 10:46:58 -0400

    Make sure the required rules are enabled for the media keys and stuff

--- keyboards/gmmk/pro/ansi/keymaps/nabeel/rules.mk
+++ keyboards/gmmk/pro/ansi/keymaps/nabeel/rules.mk
@@ -1,5 +1,8 @@
 CONSOLE_ENABLE = no
-MOUSEKEY_ENABLE = no
 IDLE_TIMEOUT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
 ENCODER_DEFAULTACTIONS_ENABLE = no
+
+# Required for the encoder knob callbacks
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
