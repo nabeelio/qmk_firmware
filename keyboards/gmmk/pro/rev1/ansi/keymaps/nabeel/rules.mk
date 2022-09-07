259050a35be7e41df565240475ec28d81516be6f
Nabeel Shahzad <nshahzad@gmail.com>
2022-09-07 15:40:05 -0500

    Set DEBOUNCE_TYPE to sym_defer_pk

--- keyboards/gmmk/pro/rev1/ansi/keymaps/nabeel/rules.mk
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/nabeel/rules.mk
@@ -2,6 +2,7 @@ CONSOLE_ENABLE = no
 IDLE_TIMEOUT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
 ENCODER_DEFAULTACTIONS_ENABLE = no
+DEBOUNCE_TYPE = sym_defer_pk
 
 # Required for the encoder knob callbacks
 MOUSEKEY_ENABLE = yes       # Mouse keys
